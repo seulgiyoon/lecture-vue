83d29b8c124f16426ae3faab717fe3f8eb65f5d5
SEULGI YOON <sgyoon.info@gmail.com>
2020-04-16 17:34:49 +0900

    검색창에서 엔터키를 입력 시 검색어가 결과 view로 넘어가도록 메인 컨트롤러와 연동함

--- 1-vanilla/js/controllers/MainController.js
+++ 1-vanilla/js/controllers/MainController.js
@@ -5,5 +5,16 @@ const tag = '[MainController]'
 export default {
   init() {
     FormView.setup(document.querySelector('form'))
+      .on('@submit', (e) => this.onSubmit(e.detail.input))
+      // formView에서 @submit 이벤트가 발생했을 때 어떠한 동작을 하도록
+      // .on 함수를 이용해서 정의한다.
+      // .on 함수를 .setup다음에 바로 이어서(체이닝으로) 사용하려면, setup메서드에서
+      // this를 리턴해주어야한다. 그래야 .on이 그걸 받아 사용할 수 있음.
+      // 커스텀 이벤트 @submit으로부터는 이벤트 객체가 넘어온다.
+      // 그 안에 detail 값 안에 @submit에서 두번째 인자로 넘긴 데이터가 담겨있음.
   },
+
+  onSubmit(query) {
+    console.log(query);
+  }
 }
--- 1-vanilla/js/views/FormView.js
+++ 1-vanilla/js/views/FormView.js
@@ -10,6 +10,8 @@ FormView.setup = function (el) {
   this.resetEl = el.querySelector('[type=reset')
   this.showResetBtn(false)
   this.bindEvents()
+  // 다른 함수를 .setup다음에 바로 이어서(체이닝으로) 사용하려면, setup메서드에서
+  // this를 리턴해주어야한다. 그래야 다음에 이어진 함수가 그걸 받아 사용할 수 있음.
   return this
 }
 
@@ -18,12 +20,23 @@ FormView.showResetBtn = function (show = true) {
 }
 
 FormView.bindEvents = function () {
+  // form 엘리먼트에 submit 이벤트가 발생했을 때 기본 이벤트 발생을 막는다.
+  // preventDefault를 안하면 그냥 엔터 쳤을 때 자동으로 이벤트가 발생하는데.
   this.on('submit', e => e.preventDefault())
   this.inputEl.addEventListener('keyup', e => this.onKeyup(e))
 }
 
 FormView.onKeyup = function (e) {
+  const enter = 13;
   this.showResetBtn(this.inputEl.value.length)
+  if (e.keyCode !== enter) return;
+
+  // 검색폼에서 엔터키로 이벤트가 발생했을 경우 검색결과를 띄우려고 함
+  // FormView의 역할은 검색폼에 대한 것 뿐, 검색 결과를 띄우는 것은 아니므로
+  // 엔터키를 누른 이벤트가 발생했음을 Main Controller에 전달만 해주면 된다.
+  // 그럼 메인 컨트롤러가 ResultView에 그를 전달한다.
+  // 그를 위해서 emit함수에 커스텀 이벤트인 @sumit을
+  this.emit('@submit', { input: this.inputEl.value });
 }
 
 export default FormView
