565698f4cb1378fa33b60905767605e053d02e80
SEULGI YOON <sgyoon.info@gmail.com>
2020-04-21 16:19:44 +0900

    검색 일자와 검색어 삭제 버튼을 보여주기

--- 1-vanilla/js/views/HistoryView.js
+++ 1-vanilla/js/views/HistoryView.js
@@ -6,4 +6,16 @@ const HistoryView = Object.create(KeywordView)
 
 HistoryView.messages.NO_KEYWORDS = '검색 이력이 없습니다'
 
+// KeywordView에서 함수를 render와 getKeywordsHtml로 나눠놓았기에 여기에서 오버라이딩 할 수 있다.
+HistoryView.getKeywordsHtml = function (data) {
+  return data.reduce((html, item) => {
+    html += `<li data-keyword="${item.keyword}">
+      ${item.keyword}
+      <span class="date">${item.date}</span>
+      <button class="btn-remove"></button>
+    </li>`
+    return html
+  }, '<ul class="list">') + "</ul>"
+}
+
 export default HistoryView
